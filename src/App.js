f8bec136efebd5ace250c9b9a1b2a3b8be51492a
Hannes Schütz <hannes@fam-schuetz.de>
2018-06-29 12:12:42 +0200

    remove unnecessary stuff

--- src/App.js
+++ src/App.js
@@ -11,39 +11,16 @@ import TodoInputContainer from './TodoInput'
 import TodoListContainer from './TodoList'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      todos: []
-    }
-
-    this.onAddTodo = this.onAddTodo.bind(this)
-    this.onRemoveTodo = this.onRemoveTodo.bind(this)
-  }
-
-  onAddTodo(item){
-    item.id = Shortid.generate()
-    let todo = {
-      id: Shortid.generate(),
-      text: item.text
-    }
-    let todos = this.state.todos
-    todos.push(todo)
-    this.setState({todos})
-  }
-
-  onRemoveTodo(id){
-    let todos = this.state.todos.filter(todo => todo.id !== id)
-    this.setState({todos})
-  }
-
+  // constructor(props) {
+  //   super(props)
+  // }
 
   render() {
     return (
       <div className="App">
         <TodoInputContainer store={store}/>
         <Provider store={store}>
-        <TodoListContainer/>
+          <TodoListContainer/>
         </Provider>
     </div>
     );
